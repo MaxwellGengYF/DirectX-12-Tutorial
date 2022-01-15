a6e48297773dc53733ecb4c618b1a063260c07b9
MaxwellGengYF <18963092533@163.com>
2022-01-15 16:13:16 +0800

    Chapter 1

---
+++ Chapter1/src/DXSample.h
@@ -0,0 +1,63 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+#pragma once
+
+#include "DXSampleHelper.h"
+#include "Win32Application.h"
+
+class DXSample
+{
+public:
+    DXSample(uint32_t width, uint32_t height, std::wstring name);
+    virtual ~DXSample();
+
+    virtual void OnInit() = 0;
+    virtual void OnUpdate() = 0;
+    virtual void OnRender() = 0;
+    virtual void OnDestroy() = 0;
+
+    // Samples override the event handlers to handle specific messages.
+    virtual void OnKeyDown(UINT8 /*key*/)   {}
+    virtual void OnKeyUp(UINT8 /*key*/)     {}
+
+    // Accessors.
+    uint32_t GetWidth() const           { return m_width; }
+    uint32_t GetHeight() const          { return m_height; }
+    const wchar_t* GetTitle() const   { return m_title.c_str(); }
+
+    void ParseCommandLineArgs(_In_reads_(argc) wchar_t* argv[], int argc);
+
+protected:
+    std::wstring GetAssetFullPath(LPCWSTR assetName);
+
+    void GetHardwareAdapter(
+        _In_ IDXGIFactory1* pFactory,
+        _Outptr_result_maybenull_ IDXGIAdapter1** ppAdapter,
+        bool requestHighPerformanceAdapter = false);
+
+    void SetCustomWindowText(LPCWSTR text);
+
+    // Viewport dimensions.
+    uint32_t m_width;
+    uint32_t m_height;
+    float m_aspectRatio;
+
+    // Adapter info.
+    bool m_useWarpDevice;
+
+private:
+    // Root assets path.
+    std::wstring m_assetsPath;
+
+    // Window title.
+    std::wstring m_title;
+};
