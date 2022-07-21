ebd291e0ebf5c6b6fb42ce542901d3c9d8871ea0
Maxwell Geng <maxwellgeng@outlook.com>
2022-07-21 15:54:37 +0800

    reset xmake & clangd settings

--- Chapter1/src/xmake.lua
+++ Chapter1/src/xmake.lua
@@ -1,22 +1,28 @@
 BuildProject({
-    projectName = "DX12_BetterSimpleBox",
+    projectName = "DX12_Triangle",
     projectType = "binary",
-    macros = {"_XM_NO_INTRINSICS_=1", "NOMINMAX", "UNICODE", "m128_f32=vector4_f32", "m128_u32=vector4_u32"},
-    debugMacros = {"_DEBUG"},
-    releaseMacros = {"NDEBUG"},
-    files = {"**.cpp"},
-    includePaths = {"./"},
-    debugException = true,
-    releaseException = true
+    event = function()
+        add_defines("_XM_NO_INTRINSICS_=1", "NOMINMAX", "UNICODE", "m128_f32=vector4_f32", "m128_u32=vector4_u32")
+        add_files("**.cpp")
+        add_includedirs("./")
+        add_links("User32", "kernel32", "Gdi32", "Shell32", "DXGI", "D3D12", "D3DCompiler")
+        after_build(function(target)
+            build_path = ""
+            src_path = "shader/"
+            if is_mode("release") then
+                build_path = "$(buildir)/windows/x64/release/"
+            else
+                build_path = "$(buildir)/windows/x64/debug/"
+            end
+            os.cp(src_path .. "*", build_path .. "shader/")
+        end)
+
+    end,
+    debugEvent = function()
+        add_defines("_DEBUG")
+    end,
+    releaseEvent = function()
+        add_defines("NDEBUG")
+    end,
+    exception = true
 })
-add_links("User32", "kernel32", "Gdi32", "Shell32", "DXGI", "D3D12", "D3DCompiler")
-after_build(function(target)
-    build_path = ""
-    src_path = "shader/"
-    if is_mode("release") then
-        build_path = "$(buildir)/windows/x64/release/"
-    else
-        build_path = "$(buildir)/windows/x64/debug/"
-    end
-    os.cp(src_path .. "*", build_path .. "shader/")
-end)
--- Chapter3/xmake.lua
+++ Chapter3/xmake.lua
@@ -1,30 +1,34 @@
+set_policy("build.ccache", false)
 add_rules("mode.release", "mode.debug")
-function Execute(map, func)
-    if map ~= nil then
-        func(map)
+function GetValue(funcOrValue)
+    if type(funcOrValue) == 'function' then
+        return funcOrValue()
+    else
+        return funcOrValue
     end
 end
+
 function SetException(enableException)
-    if (enableException ~= nil) and (enableException) then
+    local value = GetValue(enableException)
+    if (value ~= nil) and value then
         add_cxflags("/EHsc", {
             force = true
         })
     end
 end
 function BuildProject(config)
-    target(config.projectName)
-    
+    local projectName = GetValue(config.projectName)
+    if projectName == nil then
+        return
+    end
+    target(projectName)
     set_languages("clatest")
     set_languages("cxx20")
-    projectType = config.projectType
+    local projectType = GetValue(config.projectType)
     if projectType ~= nil then
         set_kind(projectType)
     end
-    Execute(config.macros, add_defines)
-    Execute(config.files, add_files)
-    Execute(config.includePaths, add_includedirs)
-    Execute(config.depends, add_deps)
-    unityBuildBatch = config.unityBuildBatch
+    local unityBuildBatch = GetValue(config.unityBuildBatch)
     if (unityBuildBatch ~= nil) and (unityBuildBatch > 0) then
         add_rules("c.unity_build", {
             batchsize = unityBuildBatch
@@ -33,28 +37,36 @@ function BuildProject(config)
             batchsize = unityBuildBatch
         })
     end
+    SetException(config.exception)
     if is_mode("release") then
         set_optimize("aggressive")
         if is_plat("windows") then
             set_runtimes("MD")
-            add_cxflags("/Zi", "/W0", "/MP", "/Ob2", "/Oi", "/Ot", "/Oy", "/GT", "/GF", "/GS-", "/Gy", "/arch:AVX2",
-                "/Gr", "/sdl-", "/GL", "/Zc:preprocessor", "/Gr", "/TP", {
-                    force = true
-                })
-            SetException(config.releaseException)
         end
-        Execute(config.releaseMacros, add_defines)
+        add_cxflags("/Zi", "/W0", "/Ob2", "/Oi", "/Ot", "/Oy", "/GT", "/GF", "/GS-", "/Gy", "/arch:AVX2", "/Gd",
+            "/sdl-", "/GL", "/Zc:preprocessor", "/TP", {
+                force = true
+            })
+        local event = GetValue(config.releaseEvent)
+        if (type(event) == "function") then
+            event()
+        end
     else
         set_optimize("none")
         if is_plat("windows") then
             set_runtimes("MDd")
-            add_cxflags("/Zi", "/W0", "/MP", "/Ob0", "/Oy-", "/GF", "/GS", "/arch:AVX2", "/Gr", "/TP", "/Gr",
-                "/Zc:preprocessor", {
-                    force = true
-                })
-            SetException(config.debugException)
         end
-        Execute(config.debugMacros, add_defines)
+        add_cxflags("/Zi", "/W0", "/Ob0", "/Oy-", "/GF", "/GS", "/arch:AVX2", "/TP", "/Gd", "/Zc:preprocessor", {
+            force = true
+        })
+        local event = GetValue(config.debugEvent)
+        if (type(event) == "function") then
+            event()
+        end
+    end
+    local event = GetValue(config.event)
+    if (type(event) == "function") then
+        event()
     end
 end
 add_subdirs("src/")
